2b397a9b9ab53e8e7013ad41172c25cb2b58cd08
Lueles <lueles-1234@hotmail.com>
2020-02-02 16:57:21 -0300

    Projeto criado

---
+++ src/test/java/com/hashmd5/cursosmc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashmd5.cursosmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
